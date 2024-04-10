534ace896a7ca062a16e7abbd82318d79ce2b198
Skatturinn <elias.ludviksson@gmail.com>
2024-04-10 16:00:49 +0000

    fra raspberry til git

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { dirname, join } = require('path');
 const { fileURLToPath } = require('url');
 
-const path = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 
 
